57a3f9f9bcb60c1d34c78c28d45e0e974c5fa99e
vinsent <vintsingle@163.com>
2026-04-05 18:23:19 +0800

    Can jump

--- algorithm/main.swift
+++ algorithm/main.swift
@@ -39,3 +39,5 @@ print("Split linked list in parts: \(splitListToParts([1, 2, 3].toListNode(), 5)
 
 let duplicateTree = [1,2,3,4,nil,2,4,nil,nil,4,].toTreeNode()
 print("Find duplicate subtrees: \(findDuplicateSubtrees(duplicateTree))")
+
+print("Can jump: \(canJump([2,3,1,1,4]))")
