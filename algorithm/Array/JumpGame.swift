9a34492493443c8172cfaf5ca1c59433698ff9e4
vinsent <vintsingle@163.com>
2026-04-06 19:54:26 +0800

    Jump game II

--- algorithm/Array/JumpGame.swift
+++ algorithm/Array/JumpGame.swift
@@ -18,8 +18,9 @@ import Foundation
  Input: nums = [2,3,1,1,4]
  Output: true
  Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ 
+ 
  Example 2:
-
  Input: nums = [3,2,1,0,4]
  Output: false
  Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
@@ -33,4 +34,38 @@ func canJump(_ nums: [Int]) -> Bool {
         if reach >= nums.count { return true }
     }
     return false
+}
+
+/*
+ You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
+
+ Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at index i, you can jump to any index (i + j) where:
+
+ 0 <= j <= nums[i] and
+ i + j < n
+ Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
+
+  
+
+ Example 1:
+ Input: nums = [2,3,1,1,4]
+ Output: 2
+ Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ 
+ Example 2:
+ Input: nums = [2,3,0,1,4]
+ Output: 2
+ */
+
+func jump(_ nums: [Int]) -> Int {
+    var i = 0, pos = 0, reach = 0, step = 0
+    while i < nums.count - 1 {
+        reach = max(reach, nums[i] + i)
+        if i == pos {
+            pos = reach
+            step += 1
+        }
+        i += 1
+    }
+    return step
 }
--- algorithm/main.swift
+++ algorithm/main.swift
@@ -40,4 +40,4 @@ print("Split linked list in parts: \(splitListToParts([1, 2, 3].toListNode(), 5)
 let duplicateTree = [1,2,3,4,nil,2,4,nil,nil,4,].toTreeNode()
 print("Find duplicate subtrees: \(findDuplicateSubtrees(duplicateTree))")
 
-print("Can jump: \(canJump([2,3,1,1,4]))")
+print("Can jump: \(canJump([2,3,1,1,4])), step: \(jump([2,3,1,1,4]))")
